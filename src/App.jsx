3f5ff40ee828d9a5ae4fa6a13e84480afe3cad1d
iFamished <me@ifamished.com>
2026-06-23 03:25:05 +0000

    Fix routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,10 @@
+import { Routes, Route, Navigate } from "react-router-dom"
 import { Navbar, Footer } from "ifamished-ui"
+
+import Home from "./pages/Home"
+import About from "./pages/About"
+import Projects from "./pages/Projects"
+import Contact from "./pages/Contact"
 
 const navItems = [
   { to: "/", label: "Home", end: true },
@@ -23,7 +29,13 @@ export default function App() {
       />
 
       <div className="container">
-        {/* routes */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
       </div>
 
       <Footer
